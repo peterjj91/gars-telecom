e4ad9fe23156bdf2aa68e19c930fe0dfbcc055c5
ppopeskul <pasha.9191@mail.ru>
2017-11-13 23:45:37 +0300

    popup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -38,12 +38,74 @@ $(function () {
 });
 
 $(function () {
-    $(".scrollbar").mCustomScrollbar({
-        scrollButtons:{enable:true},
-        theme:"light-thick",
-        scrollbarPosition:"outside"
+    if(window.matchMedia('(min-width: 850px)').matches) {
+        $(".scrollbar").mCustomScrollbar({
+            scrollButtons:{enable:true},
+            theme:"light-thick",
+            scrollbarPosition:"outside"
+        });
+    } else if (window.matchMedia('(max-width: 851px)').matches) {
+        $(".scrollbar").mCustomScrollbar({
+            axis:"x",
+            theme:"light-thick",
+            // autoExpandScrollbar:true,
+            advanced:{autoExpandHorizontalScroll:true}
+        });
+    }
+});
+
+
+$(function () {
+    $(document).ready(function(){
+        var slideout = new Slideout({
+            'panel': document.getElementById('main'),
+            'menu': document.getElementById('navbar-main'),
+            'padding': 320,
+            'tolerance': 70,
+            'side': 'right',
+            'touch': false
+        });
+
+        if(window.matchMedia('(min-width: 751px)').matches) {
+
+        } else if (window.matchMedia('(max-width: 750px)').matches) {
+
+        }
+
+        // Toggle button
+        document.querySelector('.toggle-menu').addEventListener('click', function() {
+            slideout.toggle();
+            $("#navbar-main").toggleClass("zindex-top");
+        });
+
+        document.querySelector('.sidemenu__close').addEventListener('click', function() {
+            slideout.close();
+            $("#navbar-main").removeClass("zindex-top");
+        });
+
+        function close(eve) {
+            eve.preventDefault();
+            slideout.close();
+        }
+
+        slideout
+            .on('beforeopen', function() {
+                this.panel.classList.add('slideout-open');
+                $(".header").addClass('header--open');
+                $(".burger__button").addClass("open");
+            })
+            .on('open', function() {
+                this.panel.addEventListener('click', close);
+            })
+            .on('beforeclose', function() {
+                this.panel.classList.remove('slideout-open');
+                $(".header").removeClass('header--open');
+                $(".burger__button").removeClass("open");
+                this.panel.removeEventListener('click', close);
+            });
     });
 });
+
 
 $(document).on('click','.promo-drop__icon',function(){
     $(this).toggleClass("promo-drop__icon--active");
